dd8ee854d0a9bb0e91770ea7e591c64d30864867
Imad Ahmed <j166a775433@gmail.com>
2026-03-17 13:35:44 +0000

    Complete Task 8

---
+++ Vehicles/Engine.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enginespace
+{
+    public enum FuelTypeEnum
+    {
+        Unleaded,
+        Leaded,
+        Diesel
+    }
+    public abstract class Engine
+    {
+        bool Running { get; set; }
+        int Horsepower { get; set; }
+        FuelTypeEnum FuelType { get; set; }
+
+        public Engine(int horsepower, FuelTypeEnum fuelType)
+        {
+            Running = false;
+            Horsepower = horsepower;
+            FuelType = fuelType;
+
+        }
+
+        public void Start()
+        {
+            Running = true;
+        }
+
+        
+
+
+    }
+
+    public class CarEngine : Engine
+    {
+        public CarEngine()
+            : base(600, FuelTypeEnum.Unleaded)
+        {
+
+        }
+    }
+
+    public class MotorcycleEngine : Engine
+    {
+        public MotorcycleEngine()
+            : base(1000, FuelTypeEnum.Leaded)
+        {
+
+        }
+    }
+
+    
+
+
+
+
+
+}
--- Vehicles/Vehicle.cs
+++ Vehicles/Vehicle.cs
@@ -3,14 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Enginespace;
 
 namespace Vehicles
 {
-    internal abstract class Vehicle
+    public abstract class Vehicle
     {
-        public string Make;
-        public string Model;
-        public int Speed;
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Speed { get; set; }
+        public Engine EngineType { get; set; }
+
+        public Vehicle(string make, string model, int speed, Engine engineType)
+        {
+            Make = make;
+            Model = model;
+            Speed = speed;
+            EngineType = engineType;
+        }
+
+        public void StartEngine()
+        {
+            Engine.Start();
+        }
 
     }
+
+    public class Car : Vehicle
+    {
+        public Car(string make, string model, int speed, Engine engineType)
+            : base(make, model, speed, engineType)
+        {
+
+        }
+    }
+
+    public class Motorcycle : Vehicle
+    {
+
+        string HasSideCar { get; set; }
+        public Motorcycle(string make, string model, int speed, string hasSideCar, Engine engineType)
+            : base(make, model, speed, engineType)
+        {
+            HasSideCar = hasSideCar;
+        }
+
+    }
+
+
+
+
 }
