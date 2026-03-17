88c4cdf9782a31a683d87f6bbe88e5f512c1774c
Imad Ahmed <j166a775433@gmail.com>
2026-03-17 17:34:00 +0000

    Complete problem 2...almost

---
+++ Shapes/Shape.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public abstract class Shape
+    {
+        public abstract double CalculateArea();
+    }
+
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+    public class Rectangle : Shape
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+
+        public Rectangle(double length, double width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        public override double CalculateArea()
+        {
+            return Length * Width;
+        }
+    }
+    public class Triangle : Shape
+    {
+        public double BaseLength { get; set; }
+        public double Height { get; set; }
+
+        public Triangle(double baseLength, double height)
+        {
+            BaseLength = baseLength;
+            Height = height;
+        }
+
+        public override double CalculateArea()
+        {
+            return BaseLength * Height * 0.5;
+        }
+    }
+}
---
+++ Shapes/Shape3D.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public abstract class Shape3D
+    {
+        public Shape BaseShape { get; set; }
+
+        public Shape3D(Shape baseShape)
+        {
+            BaseShape = baseShape;
+        }
+
+        public abstract double CalculateVolume();
+    }
+
+    public class Cylinder : Shape3D 
+    {
+        public double Height { get; set; }
+
+        public Cylinder(Circle baseShape, double height)
+            : base(baseShape)
+        {
+            Height = height;
+        }
+
+        public override double CalculateVolume()
+        {
+            
+            return BaseShape.CalculateArea() * Height;
+        }
+    }
+    public class Cone : Shape3D 
+    {
+        public double Height { get; set; }
+        public Cone(Circle shape, double height)
+            : base(shape)
+        {
+            Height = height;
+        }
+
+        public override double CalculateVolume()
+        {
+            return (BaseShape.CalculateArea() * Height) / 3;
+        }
+    }
+
+    public class Sphere : Shape3D
+    {
+        public Sphere(Circle baseShape)
+            : base(baseShape)
+        {
+        }
+        public override double CalculateVolume()
+        {
+            Circle circle = (Circle)BaseShape;
+            double radius = circle.Radius;
+
+            Console.WriteLine(radius);
+            Console.WriteLine(Math.Pow(5, 3) * Math.PI * (4 / 3));
+
+            return (Math.Pow(radius, 3) * Math.PI * (4 / 3));
+        }
+    }
+
+    public class Pyramid : Shape3D
+    {
+        public double Height { get; set; }
+        public Pyramid(Triangle shape, double height)
+            : base(shape)
+        {
+            Height = height;
+        }
+        public override double CalculateVolume()
+        {
+            return (BaseShape.CalculateArea() * Height) / 3;
+        }
+    }
+
+    public class Prism : Shape3D
+    {
+        public double Height { get; set; }
+        public Prism(Triangle shape, double height)
+            : base(shape)
+        {
+            Height = height;
+        }
+        public override double CalculateVolume()
+        {
+            return BaseShape.CalculateArea() * Height;
+        }
+    }
+
+
+}
